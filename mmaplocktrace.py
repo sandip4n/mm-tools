7d14dd3adc705f2b720c6fa8ea0b7417bd7d751a
Sandipan Das <sandipan@linux.ibm.com>
2021-04-09 01:13:36 +0530

    mmaplocktrace: simplify state tracking
    Signed-off-by: Sandipan Das <sandipan@linux.ibm.com>

--- mmaplocktrace.py
+++ mmaplocktrace.py
@@ -23,16 +23,12 @@ struct mmap_lock_data_t {
         struct mm_struct *mm;
     } key;
 
-    unsigned int n_tries;
-    unsigned int n_fails;
-
+    unsigned int n_attempts;
     unsigned long ts_last_start;
     unsigned long ts_last_acquire;
     unsigned long ts_last_release;
-
     unsigned long lat_acquire;
     unsigned long lat_release;
-
     char comm[TASK_COMM_LEN];
 };
 
@@ -69,16 +65,13 @@ TRACEPOINT_PROBE(mmap_lock, mmap_lock_start_locking)
         /* first attempt to acquire */
         data.key = key;
         data.ts_last_start = bpf_ktime_get_ns();
-        data.n_tries = 1;
+        data.n_attempts++;
         mmap_lock_map.insert(&data.key, &data);
-    } else if (prev->n_tries == prev->n_fails) {
+    } else if (prev->n_attempts > 0) {
         /* any previous attempts to acquire */
         prev->ts_last_start = bpf_ktime_get_ns();
-        prev->n_tries++;
+        prev->n_attempts++;
         mmap_lock_map.update(&prev->key, prev);
-    } else {
-        mmap_lock_anomaly_events.perf_submit(args, prev, sizeof(*prev));
-        //mmap_lock_map.delete(&prev->key);
     }
 
     return 0;
@@ -101,22 +94,14 @@ TRACEPOINT_PROBE(mmap_lock, mmap_lock_acquire_returned)
         return 0;
 
     prev = mmap_lock_map.lookup(&key);
-    if (!prev)
-        return 0;
-
-    if (prev->n_tries == (prev->n_fails + 1)) {
+    if (prev && prev->n_attempts > 0) {
         /* any previous attempts to acquire */
         if (args->success) {
             prev->ts_last_acquire = bpf_ktime_get_ns();
             prev->lat_acquire += prev->ts_last_acquire - prev->ts_last_start;
-            prev->lat_acquire /= prev->n_tries;
-        } else {
-            prev->n_fails++;
+            prev->lat_acquire /= prev->n_attempts;
+            mmap_lock_map.update(&prev->key, prev);
         }
-        mmap_lock_map.update(&prev->key, prev);
-    } else {
-        //mmap_lock_anomaly_events.perf_submit(args, prev, sizeof(*prev));
-        //mmap_lock_map.delete(&prev->key);
     }
 
     return 0;
@@ -139,32 +124,18 @@ TRACEPOINT_PROBE(mmap_lock, mmap_lock_released)
         return 0;
 
     prev = mmap_lock_map.lookup(&key);
-    if (!prev)
-        return 0;
-
-    if (prev->n_tries == (prev->n_fails + 1)) {
+    if (prev && prev->n_attempts > 0) {
         /* any previous attempts to acquire */
         prev->ts_last_release = bpf_ktime_get_ns();
         prev->lat_release = prev->ts_last_release - prev->ts_last_acquire;
         bpf_get_current_comm(&prev->comm, sizeof(prev->comm));
         mmap_lock_release_events.perf_submit(args, prev, sizeof(*prev));
-    } else {
-        //mmap_lock_anomaly_events.perf_submit(args, prev, sizeof(*prev));
-        //mmap_lock_map.delete(&prev->key);
+        mmap_lock_map.delete(&prev->key);
     }
 
-    mmap_lock_map.delete(&prev->key);
     return 0;
 }
 """
-
-parser = argparse.ArgumentParser(description="Trace mmap lock events and latency", formatter_class=argparse.RawDescriptionHelpFormatter)
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-a", action="store_true", default=False, help="capture migrations across the system")
-group.add_argument("-p", type=str, dest="progpath", metavar="PATH [ARGS]", help="capture migrations for a given program")
-args = parser.parse_args()
-
-b = BPF(text=bpf_text)
 
 class mmap_lock_key_t(ctypes.Structure):
     _fields_ = [
@@ -177,8 +148,7 @@ class mmap_lock_key_t(ctypes.Structure):
 class mmap_lock_data_t(ctypes.Structure):
     _fields_ = [
         ('key', mmap_lock_key_t),
-        ('n_tries', ctypes.c_uint),
-        ('n_fails', ctypes.c_uint),
+        ('n_attempts', ctypes.c_uint),
         ('ts_last_start', ctypes.c_ulong),
         ('ts_last_acquire', ctypes.c_ulong),
         ('ts_last_release', ctypes.c_ulong),
@@ -187,30 +157,37 @@ class mmap_lock_data_t(ctypes.Structure):
         ('comm', ctypes.c_char * 16),
     ]
 
+parser = argparse.ArgumentParser(
+            description="Trace mmap lock events and latency",
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-a", action="store_true", default=False,
+                   help="capture migrations across the system")
+group.add_argument("-p", type=str, dest="progpath", metavar="PATH [ARGS]",
+                   help="capture migrations for a given program")
+args = parser.parse_args()
+
 def print_mmap_lock_release_event(cpu, data, size):
     event = ctypes.cast(data, ctypes.POINTER(mmap_lock_data_t)).contents
-    print("release: %-16.16s pid %-6s tgid %-6s %-8s tries %-8s fails %-8s latency (us) acquire %-10.3f release %-10.3f" % (
+    print("%-16.16s %8s %8s %8s %10s %24.3f %24.3f" % (
           event.comm.decode("utf-8", "replace"),
           event.key.pid, event.key.tgid,
-          "write" if event.key.is_write else "read",
-          event.n_tries, event.n_fails,
+          "write" if event.key.is_write else "read", event.n_attempts,
           event.lat_acquire / 1000, event.lat_release / 1000))
 
-def print_mmap_lock_anomaly_event(cpu, data, size):
-    event = ctypes.cast(data, ctypes.POINTER(mmap_lock_data_t)).contents
-    error = "unknown error"
-    if (event.ts_last_acquire > 0 and event.ts_last_release == 0):
-        error = "attempt to re-acquire before release"
-    print("anomaly: %-16.16s pid %-6s tgid %-6s mm %016x tries %-8s fails %-8s %s" % (
-          event.comm.decode("utf-8", "replace"),
-          event.key.pid, event.key.tgid, event.key.mm,
-          event.n_tries, event.n_fails, error))
+b = BPF(text=bpf_text)
+b["mmap_lock_release_events"].open_perf_buffer(print_mmap_lock_release_event,
+                                               page_cnt=64)
 
-b["mmap_lock_release_events"].open_perf_buffer(print_mmap_lock_release_event, page_cnt=64)
-b["mmap_lock_anomaly_events"].open_perf_buffer(print_mmap_lock_anomaly_event, page_cnt=64)
+print("%-16.16s %8s %8s %8s %10s %24s %24s" % (
+      "COMM", "PID", "TGID", "TYPE", "ATTEMPTS",
+      "ACQUIRE LATENCY (us)", "RELEASE LATENCY (us)"))
 
 if args.progpath:
-    p = subprocess.Popen(args.progpath.split(), shell=False, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    p = subprocess.Popen(args.progpath.split(), shell=False,
+                         stdin=subprocess.DEVNULL,
+                         stdout=subprocess.DEVNULL,
+                         stderr=subprocess.DEVNULL)
     b["pid_map"][ctypes.c_uint(0)] = ctypes.c_uint(p.pid)
 
 while True:
